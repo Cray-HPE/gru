2b54bfba70f14154772c82431fa7bb7b9242abc9
Jacob Salmela <jacob.salmela@hpe.com>
2023-10-31 14:12:15 -0500

    add uefi boot order control for MTL-2229
    Signed-off-by: Jacob Salmela <jacob.salmela@hpe.com>

--- pkg/cmd/cli/types.go
+++ pkg/cmd/cli/types.go
@@ -42,3 +42,18 @@ type State struct {
 	PowerState redfish.PowerState `json:"powerState"`
 	Error      error              `json:"error,omitempty"`
 }
+
+// Boot represents boot configuration on the BMC. Only Error is emitted on empty.
+type Boot struct {
+	Order []BootDescription `json:"order"`
+	Next  string            `json:"next"`
+	Error error             `json:"error,omitempty"`
+}
+
+type BootDescription map[string]string
+
+// Override represents the result of the boot override.
+type Override struct {
+	Target redfish.BootSourceOverrideTarget `json:"target"`
+	Error  error                            `json:"error,omitempty"`
+}
